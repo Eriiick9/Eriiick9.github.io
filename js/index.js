251aee1db883ebc81e46a7c845544fe34aa811aa
erick <155974974+Eriiick9@users.noreply.github.com>
2024-04-11 10:00:13 +0100

    menu

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// const erick = document.querySelector('.menu2');
+// const menuList = document.querySelector('.lista2');
+const dropMenu = document.querySelector('.menu2');
+const alerta = "funciona";
+
+dropMenu.addEventListener('click', function() {
+    alert(alerta);
+
+});
+
