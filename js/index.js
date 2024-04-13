2035bd96c442ec607b01c40fb8b01c2b331da531
erick <155974974+Eriiick9@users.noreply.github.com>
2024-04-13 18:53:50 +0100

    botones-menu

--- js/index.js
+++ js/index.js
@@ -1,13 +1,1 @@
-const erick = document.querySelector('.menu2');
-const menuList = document.querySelector('.menu2 img');
-const dropMenu = document.querySelector('.lista2');
-
-erick.onclick = function () {
-    dropMenu.classList.erick('open')
-    const hola = dropMenu.classList.contains('open')
-
-    menuList.classList = hola
-    ?
-    :
-}
-
+const menuImg = Document.querySelector('.manu2')
