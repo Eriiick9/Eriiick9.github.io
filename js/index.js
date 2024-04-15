e65d4585aaadc58a2843f6bdc438ac64bdb3d02c
erick <155974974+Eriiick9@users.noreply.github.com>
2024-04-15 15:09:25 +0100

    falta responsive de desktop

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 const menuImg = document.querySelector('.menu2');
 const dropMenu = document.querySelector('.drop-menu');
+const menuImgX = document.querySelector('.menu2X');
 
 menuImg.onclick = function() {
     dropMenu.classList.toggle('open');
+    menuImg.style.display = "none"; // Oculta el botón de menú
+    menuImgX.style.display = "block"; // Muestra el botón de cerrar menú
+    menuImgX.style.animationName = "rebote"; // Aplica la animación de rebote
 };
+
+menuImgX.onclick = function() {
+    dropMenu.classList.toggle('open');
+    menuImg.style.display = "block"; // Muestra el botón de menú
+    menuImgX.style.display = "none"; // Oculta el botón de cerrar menú
+    menuImg.style.animationName = "rebote"; // Aplica la animación de rebote
+};
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var animacion = document.querySelector('.bloque');
+    
+//     animacion.addEventListener('animationend', function() {
+//       animacion.classList.add('finalizado');
+//     });
+//   });
